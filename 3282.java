6d9d0e606227089f32cde3bb9e5532ae1735e699
Suryaprasad04 <150020783+Suryaprasad04@users.noreply.github.com>
2025-05-19 14:31:05 +0530

    Update 3282.java

--- 3282.java
+++ 3282.java
@@ -17,3 +17,21 @@ class Solution {
         return sum;
     }
 }
+
+
+class Solution {
+    public long findMaximumScore(List<Integer> nums) {
+        long totalScore = 0;
+        int maxSoFar = 0;
+
+        // Iterate through the array up to the second-to-last element
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // Update maxSoFar if current element is greater
+            maxSoFar = Math.max(maxSoFar, nums.get(i));
+            // Add maxSoFar to the total score
+            totalScore += maxSoFar;
+        }
+
+        return totalScore;
+    }
+}
